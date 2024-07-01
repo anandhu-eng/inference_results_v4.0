48951f77a2c8882ae2bb76592bbf04ce83618d41
ANANDHU S <71482562+anandhu-eng@users.noreply.github.com>
2024-07-02 01:50:15 +0530

    Reverted model file check
    The model file check could be handled through CM

--- closed/NVIDIA/code/stable-diffusion-xl/tensorrt/download_model.sh
+++ closed/NVIDIA/code/stable-diffusion-xl/tensorrt/download_model.sh
@@ -29,42 +29,35 @@ MODEL_DIR=build/models
 DATA_DIR=build/data
 
 # Download the fp16 raw weights of MLCommon hosted HF checkpoints
-# Check whether the folder already exists, if yes then do not download
-if [ ! -d "${MODEL_DIR}/SDXL/official_pytorch/fp16" ]; then
-    echo "Directory not found, proceeding with download and verification..."
+download_file models SDXL/official_pytorch/fp16 \
+    https://cloud.mlcommons.org/index.php/s/LCdW5RM6wgGWbxC/download \
+    stable_diffusion_fp16.zip
 
-    download_file models SDXL/official_pytorch/fp16 \
-        https://cloud.mlcommons.org/index.php/s/LCdW5RM6wgGWbxC/download \
-        stable_diffusion_fp16.zip
+unzip ${MODEL_DIR}/SDXL/official_pytorch/fp16/stable_diffusion_fp16.zip \
+    -d ${MODEL_DIR}/SDXL/official_pytorch/fp16/
 
-    unzip ${MODEL_DIR}/SDXL/official_pytorch/fp16/stable_diffusion_fp16.zip \
-        -d ${MODEL_DIR}/SDXL/official_pytorch/fp16/
+md5sum ${MODEL_DIR}/SDXL/official_pytorch/fp16/stable_diffusion_fp16/checkpoint_pipe/text_encoder/model.safetensors | grep "81b87e641699a4cd5985f47e99e71eeb"
+if [ $? -ne 0 ]; then
+    echo "SDXL CLIP1 fp16 model md5sum mismatch"
+    exit -1
+fi
 
-    md5sum ${MODEL_DIR}/SDXL/official_pytorch/fp16/stable_diffusion_fp16/checkpoint_pipe/text_encoder/model.safetensors | grep "81b87e641699a4cd5985f47e99e71eeb"
-    if [ $? -ne 0 ]; then
-        echo "SDXL CLIP1 fp16 model md5sum mismatch"
-        exit -1
-    fi
+md5sum ${MODEL_DIR}/SDXL/official_pytorch/fp16/stable_diffusion_fp16/checkpoint_pipe/text_encoder_2/model.safetensors | grep "5e540a9d92f6f88d3736189fd28fa6cd"
+if [ $? -ne 0 ]; then
+    echo "SDXL CLIP2 fp16 model md5sum mismatch"
+    exit -1
+fi
 
-    md5sum ${MODEL_DIR}/SDXL/official_pytorch/fp16/stable_diffusion_fp16/checkpoint_pipe/text_encoder_2/model.safetensors | grep "5e540a9d92f6f88d3736189fd28fa6cd"
-    if [ $? -ne 0 ]; then
-        echo "SDXL CLIP2 fp16 model md5sum mismatch"
-        exit -1
-    fi
+md5sum ${MODEL_DIR}/SDXL/official_pytorch/fp16/stable_diffusion_fp16/checkpoint_pipe/unet/diffusion_pytorch_model.safetensors | grep "edfa956683fb6121f717d095bf647f53"
+if [ $? -ne 0 ]; then
+    echo "SDXL UNet fp16 model md5sum mismatch"
+    exit -1
+fi
 
-    md5sum ${MODEL_DIR}/SDXL/official_pytorch/fp16/stable_diffusion_fp16/checkpoint_pipe/unet/diffusion_pytorch_model.safetensors | grep "edfa956683fb6121f717d095bf647f53"
-    if [ $? -ne 0 ]; then
-        echo "SDXL UNet fp16 model md5sum mismatch"
-        exit -1
-    fi
-
-    md5sum ${MODEL_DIR}/SDXL/official_pytorch/fp16/stable_diffusion_fp16/checkpoint_pipe/vae/diffusion_pytorch_model.safetensors | grep "25fe90074af9a0fe36d4a713ad5a3a29"
-    if [ $? -ne 0 ]; then
-        echo "SDXL VAE fp16 model md5sum mismatch"
-        exit -1
-    fi
-else
-    echo "Directory ${MODEL_DIR}/SDXL/official_pytorch/fp16 already exists. Skipping download and verification."
+md5sum ${MODEL_DIR}/SDXL/official_pytorch/fp16/stable_diffusion_fp16/checkpoint_pipe/vae/diffusion_pytorch_model.safetensors | grep "25fe90074af9a0fe36d4a713ad5a3a29"
+if [ $? -ne 0 ]; then
+    echo "SDXL VAE fp16 model md5sum mismatch"
+    exit -1
 fi
 
 # Run onnx generation script
