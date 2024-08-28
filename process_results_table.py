64da632b687bf669cc55d43341901afd80808a55
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-08-28 01:14:34 +0100

    No pager on results table

--- process_results_table.py
+++ process_results_table.py
@@ -9,7 +9,7 @@ with open('summary_results.json') as f:
 
 tableposhtml = """
 <!-- pager -->
-<div class="pager PAGER_CLASS">
+<div class="pager1 PAGER_CLASS">
             <img src="https://mottie.github.io/tablesorter/addons/pager/icons/first.png" class="first"/>
             <img src="https://mottie.github.io/tablesorter/addons/pager/icons/prev.png" class="prev"/>
             <span class="pagedisplay"></span> <!-- this can be any element, including an input -->
